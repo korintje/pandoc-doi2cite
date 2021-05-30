860eac0686775d64f32ccab71e5a058ddf010055
korintje <t.hosomi1410@gmail.com>
2021-05-30 16:43:12 +0900

    Update to v1.1

--- doi2cite.lua
+++ doi2cite.lua
@@ -9,10 +9,17 @@
 -- Global variables --
 --------------------------------------------------------------------------------
 base_url = "http://api.crossref.org"
-bibpath = "./bib_from_doi.bib"
+mailto = "pandoc.doi2cite@gmail.com"
+bibname = "__from_DOI.bib"
 key_list = {};
 doi_key_map = {};
 doi_entry_map = {};
+error_strs = {};
+error_strs["Resource not found."] = 404
+error_strs["No acceptable resource available."] = 406
+error_strs["<html><body><h1>503 Service Unavailable</h1>\n"
+        .."No server is available to handle this request.\n"
+        .."</body></html>"] = 503
 
 
 --------------------------------------------------------------------------------
@@ -20,14 +27,10 @@ doi_entry_map = {};
 --------------------------------------------------------------------------------
 -- Get bibliography filepath from yaml metadata
 function Meta(m)
-    local bp = m.bib_from_doi
-    if bp ~= nil then
-        if bp[1].text ~= nil then
-            bibpath = bp[1].text
-        elseif bp[1][1] ~= nil then
-            bibpath = bp[1][1].text
-        else end
-    end
+    local bib_data = m.bibliography
+    local bibpaths = get_paths_from(bib_data)
+    bibpath = find_filepath(bibname, bibpaths)
+    bibpath = verify_path(bibpath)
     local f = io.open(bibpath, "r")
     if f then
         entries_str = f:read('*all')
@@ -39,8 +42,9 @@ function Meta(m)
             end
         end
         f:close()
+    else
+        make_new_file(bibpath)
     end
-    print(bibpath .. " is created for bibliography from DOI.")
 end
 
 -- Get bibtex data of doi-based citation.id and make bibliography.
@@ -58,18 +62,16 @@ function Cite(c)
             doi = nil
         end
         if doi then
-            if doi_key_map[doi] ~= nil then
-                local entry_key = doi_key_map[doi]
-                citation.id = entry_key
-                print("Existing DOI: "..doi)
+            if doi_key_map[doi] then
+                citation.id = doi_key_map[doi]
             else
                 local entry_str = get_bibentry(doi)
-                if entry_str == nil or entry_str == "Resource not found." then
+                if entry_str == nil or error_strs[entry_str] then
                     print("Failed to get ref from DOI: " .. doi)
                 else
-                    entry_str = replace_symbols(entry_str)
+                    entry_str = tex2raw(entry_str)
                     local entry_key = get_entrykey(entry_str)
-                    if key_list[entry_key] ~= nil then
+                    if key_list[entry_key] then
                         entry_key = entry_key.."_"..doi
                         entry_str = replace_entrykey(entry_str, entry_key)
                     end
@@ -81,7 +83,7 @@ function Cite(c)
                         f:write(entry_str .. "\n")
                         f:close()
                     else
-                        error("Can not open file: "..bibpath)
+                        error("Unable to open file: "..bibpath)
                     end
                 end                
             end
@@ -99,21 +101,71 @@ function get_bibentry(doi)
     local entry_str = doi_entry_map[doi]
     if entry_str == nil then
         print("Request DOI: " .. doi)
-        local url = base_url.."/works/"..doi.."/transform/application/x-bibtex"
+        local url = base_url.."/works/"
+            ..doi.."/transform/application/x-bibtex"
+            .."?mailto="..mailto
         mt, entry_str = pandoc.mediabag.fetch(url)
     end
     return entry_str
 end
 
--- Replace some symbols inorder to escape (maybe) bugs of pandoc/citeproc
-function replace_symbols(string)
-    local buggystrs = {};
-    buggystrs["{\textendash}"] = "–"
-    buggystrs["{\textemdash}"] = "—"
-    buggystrs["{\textquoteright}"] = "’"
-    buggystrs["{\textquoteleft}"] = "‘"
-    for buggystr, altanative in pairs(buggystrs) do
-        local string = string:gsub(buggystr, altanative)
+-- Extract designated filepaths from 1 or 2 dimensional metadata
+function get_paths_from(metadata)
+    local filepaths = {};
+    if metadata then
+        if metadata[1].text then
+            filepaths[metadata[1].text] = true
+        elseif type(metadata) == "table" then
+            for _, datum in pairs(metadata) do
+                if datum[1] then
+                    if datum[1].text then
+                        filepaths[datum[1].text] = true
+                    end
+                end
+            end
+        end
+    end
+    return filepaths    
+end
+
+-- Extract filename and dirname from a given a path 
+function split_path(filepath)
+    local delim = nil
+    local len = filepath:len()
+    local reversed = filepath:reverse()
+    if filepath:find("/") then
+        delim = "/"
+    elseif filepath:find([[\]]) then
+        delim = [[\]]
+    else
+        return {filename = filepath, dirname = nil}
+    end
+    local pos = reversed:find(delim)
+    local dirname = filepath:sub(1, len - pos)
+    local filename = reversed:sub(1, pos - 1):reverse()
+    return {filename = filename, dirname = dirname}
+end
+
+-- Find bibname in a given filepath list and return the filepath if found 
+function find_filepath(filename, filepaths)
+    for path, _ in pairs(filepaths) do
+        local filename = split_path(path)["filename"]
+        if filename == bibname then
+            return path
+        end
+    end
+    return nil
+end
+
+-- Make some TeX descriptions processable by citeproc
+function tex2raw(string)
+    local symbols = {};
+    symbols["{\textendash}"] = "–"
+    symbols["{\textemdash}"] = "—"
+    symbols["{\textquoteright}"] = "’"
+    symbols["{\textquoteleft}"] = "‘"
+    for tex, raw in pairs(symbols) do
+        local string = string:gsub(tex, raw)
     end
     return string
 end
@@ -157,6 +209,39 @@ function get_doi_key_map(bibtex_string)
     return keys
 end
 
+-- function to make directories and files
+function make_new_file(filepath)
+    if filepath then
+        print("doi2cite: creating "..filepath)
+        local dirname = split_path(filepath)["dirname"]
+        if dirname then
+            os.execute("mkdir -p "..dirname)
+        end
+        f = io.open(filepath, "w")
+        if f then
+            f:close()
+        else
+            error("Unable to make bibtex file: "..bibpath..".\n"
+            .."This error may come from the missing directory. \n"
+            )
+        end
+    end
+end
+
+-- Verify that the given filepath is correct.
+-- Catch common Pandoc user mistakes about Windows-formatted filepath.
+function verify_path(bibpath)
+    if bibpath == nil then
+        print("[WARNING] doi2cite: "
+            .."The given file path is incorrect or empty. "
+            .."In Windows-formatted filepath, Pandoc recognizes "
+            .."double backslash ("..[[\\]]..") as the delimiters."
+        )
+        return "__from_DOI.bib"
+    else
+        return bibpath
+    end
+end
 
 --------------------------------------------------------------------------------
 -- The main function --
